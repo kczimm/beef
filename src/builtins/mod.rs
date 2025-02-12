41a53c2c247cfca77960181d7c0a7fb7ebfaf50b
Kevin Zimmerman <4733573+kczimm@users.noreply.github.com>
2025-02-12 10:46:00 -0600

    add echo builtin

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -3,6 +3,7 @@ use std::process;
 pub fn execute(command: &str, args: &[String]) -> Result<bool, String> {
     match command.trim() {
         "exit" => exit(args),
+        "echo" => echo(args),
         _ => Ok(false),
     }
 }
@@ -17,4 +18,9 @@ fn exit(args: &[String]) -> Result<bool, String> {
     };
 
     process::exit(exit_code);
+}
+
+fn echo(args: &[String]) -> Result<bool, String> {
+    print!("{}", args.join(" "));
+    Ok(true)
 }
